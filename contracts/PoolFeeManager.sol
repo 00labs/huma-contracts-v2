8b7fc374570d9f22ddb0c28b11ace36cf9338914
Jiatu <ljiatu@hotmail.com>
2023-10-18 14:14:41 +0800

    Remove console.sol import

--- contracts/PoolFeeManager.sol
+++ contracts/PoolFeeManager.sol
@@ -13,8 +13,6 @@ import {Errors} from "./Errors.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import "hardhat/console.sol";
-
 contract PoolFeeManager is PoolConfigCache, IPoolFeeManager {
     using SafeERC20 for IERC20;
 
