b89d220d112a6bbbf159bc8c750eb7458aaae582
Jiatu <ljiatu@hotmail.com>
2023-10-20 15:57:39 +0800

    Remove console.log

--- contracts/RiskAdjustedTranchesPolicy.sol
+++ contracts/RiskAdjustedTranchesPolicy.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import {BaseTranchesPolicy} from "./BaseTranchesPolicy.sol";
 import {LPConfig} from "./PoolConfig.sol";
 import {SENIOR_TRANCHE, JUNIOR_TRANCHE, HUNDRED_PERCENT_IN_BPS} from "./SharedDefs.sol";
-import "hardhat/console.sol";
 
 /**
  * @notice RiskAdjustedTranchesPolicy is one tranche policy implementation. In this policy,
@@ -19,7 +18,6 @@ contract RiskAdjustedTranchesPolicy is BaseTranchesPolicy {
         uint96[2] memory assets,
         uint256 /*lastUpdatedTime*/
     ) external view returns (uint96[2] memory newAssets) {
-        console.log("seniorAssets %d", assets[SENIOR_TRANCHE]);
         uint256 seniorAssets = assets[SENIOR_TRANCHE];
         uint256 juniorAssets = assets[JUNIOR_TRANCHE];
 
