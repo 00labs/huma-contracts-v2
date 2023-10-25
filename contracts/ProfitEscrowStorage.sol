94ca55aa62f0c849fbd38b2e0b7b4c513ba8ddeb
Jiatu <ljiatu@hotmail.com>
2023-10-25 22:50:29 +0800

    Rename rewardDebt -> profitDebt
    We've already renamed `accRewardPerShare` -> `accProfitPerShare`, so this renaming makes our naming consistent.

--- contracts/ProfitEscrowStorage.sol
+++ contracts/ProfitEscrowStorage.sol
@@ -6,22 +6,22 @@ import {IPoolSafe} from "./interfaces/IPoolSafe.sol";
 contract ProfitEscrowStorage {
     struct UserInfo {
         uint96 amount;
-        // `rewardDebt` acts as an accounting mechanism to adjust the claimable rewards for each user,
+        // `profitDebt` acts as an accounting mechanism to adjust the claimable profit for each user,
         // ensuring users can only claim profits generated while their principal was actively contributing to the pool.
         // See comments in the implementation contract for explanations of how this variable changes in value.
-        int96 rewardDebt;
+        int96 profitDebt;
     }
 
     struct EscrowInfo {
         uint96 totalAmount;
-        // `accProfitPerShare` keeps track of the accumulated rewards per share of the pool, where a share is
+        // `accProfitPerShare` keeps track of the accumulated profit per share of the pool, where a share is
         // a unit of principal contributed by a user. It's updated whenever profits are added to the pool
-        // and is used to calculate the claimable rewards for each user.
+        // and is used to calculate the claimable profit for each user.
         uint96 accProfitPerShare;
     }
 
     // The naive implementation of `ProfitEscrow` would require us to iterate over all users and compute the
-    // amount of claimable profit for each user in a loop. The use of `rewardDebt` and `accProfitPerShare`
+    // amount of claimable profit for each user in a loop. The use of `profitDebt` and `accProfitPerShare`
     // results in significant reduction in gas cost, making the contract much more scalable.
 
     address public controller;
