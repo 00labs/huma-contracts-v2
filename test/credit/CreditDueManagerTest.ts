c72503b8db7b3370b696d2d090fa6c0546ee332b
Jiatu <ljiatu@hotmail.com>
2023-11-01 23:18:41 +0800

    De-indent test cases in CreditDueManagerTest

--- test/credit/CreditDueManagerTest.ts
+++ test/credit/CreditDueManagerTest.ts
@@ -155,135 +155,135 @@ describe("CreditDueManager.sol Tests", function () {
                 frontLoadingFeeFlat,
             );
         });
+    });
 
-        describe("distBorrowingAmount", function () {
-            let frontLoadingFeeFlat: BN;
+    describe("distBorrowingAmount", function () {
+        let frontLoadingFeeFlat: BN;
 
-            async function setFrontLoadingFee() {
-                frontLoadingFeeFlat = toToken(10);
-                await poolConfigContract.connect(poolOwner).setFrontLoadingFees({
-                    frontLoadingFeeFlat,
-                    frontLoadingFeeBps: 0,
-                });
-            }
-
-            beforeEach(async function () {
-                await loadFixture(setFrontLoadingFee);
+        async function setFrontLoadingFee() {
+            frontLoadingFeeFlat = toToken(10);
+            await poolConfigContract.connect(poolOwner).setFrontLoadingFees({
+                frontLoadingFeeFlat,
+                frontLoadingFeeBps: 0,
             });
+        }
 
-            it("Should return the correct amount to the borrower and the platform fees", async function () {
-                const borrowAmount = toToken(100);
-                const amounts = await creditDueManagerContract.distBorrowingAmount(borrowAmount);
-                expect(amounts[0]).to.equal(borrowAmount.sub(frontLoadingFeeFlat));
-                expect(amounts[1]).to.equal(frontLoadingFeeFlat);
-            });
-
-            it("Should revert if the borrow amount is less than the platform fees", async function () {
-                const borrowAmount = toToken(9);
-                await expect(
-                    creditDueManagerContract.distBorrowingAmount(borrowAmount),
-                ).to.be.revertedWithCustomError(
-                    creditDueManagerContract,
-                    "borrowingAmountLessThanPlatformFees",
-                );
-            });
+        beforeEach(async function () {
+            await loadFixture(setFrontLoadingFee);
         });
 
-        describe("checkLate", function () {
-            it("Should return true if there are missed periods", async function () {
-                const creditRecord = {
-                    unbilledPrincipal: 0,
-                    nextDueDate: Date.now(),
-                    nextDue: 0,
-                    yieldDue: 0,
-                    totalPastDue: 0,
-                    missedPeriods: 1,
-                    remainingPeriods: 0,
-                    state: CreditState.Delayed,
-                };
-                expect(await creditDueManagerContract.checkLate(creditRecord)).to.be.true;
-            });
-
-            it("Should return true if there is payment due and we've already passed the payment grace period", async function () {
-                const poolSettings = await poolConfigContract.getPoolSettings();
-                // Advance next block time to be a second after the end of the late payment grace period.
-                const nextBlockTime = timestampToMoment(await getFutureBlockTime(0))
-                    .add(poolSettings.latePaymentGracePeriodInDays, "days")
-                    .add(1, "second");
-                await mineNextBlockWithTimestamp(nextBlockTime.unix());
-                const creditRecord = {
-                    unbilledPrincipal: 0,
-                    nextDueDate: moment().unix(),
-                    nextDue: toToken(1_000),
-                    yieldDue: 0,
-                    totalPastDue: 0,
-                    missedPeriods: 0,
-                    remainingPeriods: 0,
-                    state: CreditState.GoodStanding,
-                };
-                expect(await creditDueManagerContract.checkLate(creditRecord)).to.be.true;
-            });
-
-            it("Should return false if there is no missed periods and no next due", async function () {
-                const creditRecord = {
-                    unbilledPrincipal: 0,
-                    nextDueDate: 0,
-                    nextDue: 0,
-                    yieldDue: 0,
-                    totalPastDue: 0,
-                    missedPeriods: 0,
-                    remainingPeriods: 0,
-                    state: CreditState.Approved,
-                };
-                expect(await creditDueManagerContract.checkLate(creditRecord)).to.be.false;
-            });
-
-            it("Should return false if there is next due but we are not at the due date yet", async function () {
-                const nextDueDate = timestampToMoment(Date.now()).add(1, "day");
-                const creditRecord = {
-                    unbilledPrincipal: 0,
-                    nextDueDate: nextDueDate.unix(),
-                    nextDue: toToken(1_000),
-                    yieldDue: 0,
-                    totalPastDue: 0,
-                    missedPeriods: 0,
-                    remainingPeriods: 0,
-                    state: CreditState.Approved,
-                };
-                expect(await creditDueManagerContract.checkLate(creditRecord)).to.be.false;
-            });
+        it("Should return the correct amount to the borrower and the platform fees", async function () {
+            const borrowAmount = toToken(100);
+            const amounts = await creditDueManagerContract.distBorrowingAmount(borrowAmount);
+            expect(amounts[0]).to.equal(borrowAmount.sub(frontLoadingFeeFlat));
+            expect(amounts[1]).to.equal(frontLoadingFeeFlat);
         });
 
-        describe("getNextBillRefreshDate", function () {
-            // TODO(jiatu): fill this in
+        it("Should revert if the borrow amount is less than the platform fees", async function () {
+            const borrowAmount = toToken(9);
+            await expect(
+              creditDueManagerContract.distBorrowingAmount(borrowAmount),
+            ).to.be.revertedWithCustomError(
+              creditDueManagerContract,
+              "borrowingAmountLessThanPlatformFees",
+            );
+        });
+    });
+
+    describe("checkLate", function () {
+        it("Should return true if there are missed periods", async function () {
+            const creditRecord = {
+                unbilledPrincipal: 0,
+                nextDueDate: Date.now(),
+                nextDue: 0,
+                yieldDue: 0,
+                totalPastDue: 0,
+                missedPeriods: 1,
+                remainingPeriods: 0,
+                state: CreditState.Delayed,
+            };
+            expect(await creditDueManagerContract.checkLate(creditRecord)).to.be.true;
         });
 
-        describe("refreshLateFee", function () {
-            // TODO(jiatu): fill this in
+        it("Should return true if there is payment due and we've already passed the payment grace period", async function () {
+            const poolSettings = await poolConfigContract.getPoolSettings();
+            // Advance next block time to be a second after the end of the late payment grace period.
+            const nextBlockTime = timestampToMoment(await getFutureBlockTime(0))
+              .add(poolSettings.latePaymentGracePeriodInDays, "days")
+              .add(1, "second");
+            await mineNextBlockWithTimestamp(nextBlockTime.unix());
+            const creditRecord = {
+                unbilledPrincipal: 0,
+                nextDueDate: moment().unix(),
+                nextDue: toToken(1_000),
+                yieldDue: 0,
+                totalPastDue: 0,
+                missedPeriods: 0,
+                remainingPeriods: 0,
+                state: CreditState.GoodStanding,
+            };
+            expect(await creditDueManagerContract.checkLate(creditRecord)).to.be.true;
         });
 
-        describe("getDueInfo", function () {
-            // TODO(jiatu): fill this in
+        it("Should return false if there is no missed periods and no next due", async function () {
+            const creditRecord = {
+                unbilledPrincipal: 0,
+                nextDueDate: 0,
+                nextDue: 0,
+                yieldDue: 0,
+                totalPastDue: 0,
+                missedPeriods: 0,
+                remainingPeriods: 0,
+                state: CreditState.Approved,
+            };
+            expect(await creditDueManagerContract.checkLate(creditRecord)).to.be.false;
         });
 
-        describe("getPayoffAmount", function () {
-            it("Should return the payoff amount", async function () {
-                const creditRecord = {
-                    unbilledPrincipal: toToken(12_345),
-                    nextDueDate: Date.now(),
-                    nextDue: toToken(54_321),
-                    yieldDue: 0,
-                    totalPastDue: toToken(7_890),
-                    missedPeriods: 1,
-                    remainingPeriods: 0,
-                    state: CreditState.Delayed,
-                };
-                expect(await creditDueManagerContract.getPayoffAmount(creditRecord)).to.equal(
-                    creditRecord.unbilledPrincipal
-                        .add(creditRecord.nextDue)
-                        .add(creditRecord.totalPastDue),
-                );
-            });
+        it("Should return false if there is next due but we are not at the due date yet", async function () {
+            const nextDueDate = timestampToMoment(Date.now()).add(1, "day");
+            const creditRecord = {
+                unbilledPrincipal: 0,
+                nextDueDate: nextDueDate.unix(),
+                nextDue: toToken(1_000),
+                yieldDue: 0,
+                totalPastDue: 0,
+                missedPeriods: 0,
+                remainingPeriods: 0,
+                state: CreditState.Approved,
+            };
+            expect(await creditDueManagerContract.checkLate(creditRecord)).to.be.false;
+        });
+    });
+
+    describe("getNextBillRefreshDate", function () {
+        // TODO(jiatu): fill this in
+    });
+
+    describe("refreshLateFee", function () {
+        // TODO(jiatu): fill this in
+    });
+
+    describe("getDueInfo", function () {
+        // TODO(jiatu): fill this in
+    });
+
+    describe("getPayoffAmount", function () {
+        it("Should return the payoff amount", async function () {
+            const creditRecord = {
+                unbilledPrincipal: toToken(12_345),
+                nextDueDate: Date.now(),
+                nextDue: toToken(54_321),
+                yieldDue: 0,
+                totalPastDue: toToken(7_890),
+                missedPeriods: 1,
+                remainingPeriods: 0,
+                state: CreditState.Delayed,
+            };
+            expect(await creditDueManagerContract.getPayoffAmount(creditRecord)).to.equal(
+              creditRecord.unbilledPrincipal
+                .add(creditRecord.nextDue)
+                .add(creditRecord.totalPastDue),
+            );
         });
     });
 });
