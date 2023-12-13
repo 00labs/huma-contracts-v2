8ec6c8a3748ed63b8639120acaef2fe21c7addb6
shan-57blocks <115970472+shan-57blocks@users.noreply.github.com>
2023-12-13 11:17:10 +0800

    fix zero coverage issue (#249)
    * fix zero coverage issue
    
    * update coverage skip files
    
    * add typing for hardhat config object
    
    ---------
    
    Co-authored-by: shan <shan@57blocks.com>

---
+++ hardhat.base.ts
@@ -0,0 +1,38 @@
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-contract-sizer";
+
+import { HardhatUserConfig } from "hardhat/types";
+
+const config: HardhatUserConfig = {
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.18",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+        ],
+    },
+    contractSizer: {
+        alphaSort: true,
+        disambiguatePaths: false,
+        runOnCompile: true,
+        strict: true,
+    },
+    /**
+     * gas reporter configuration that lets you know
+     * an estimate of gas for contract deployments and function calls
+     * More here: https://hardhat.org/plugins/hardhat-gas-reporter.html
+     */
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        // coinmarketcap: process.env.COINMARKETCAP || null,
+    },
+};
+
+export default config;
