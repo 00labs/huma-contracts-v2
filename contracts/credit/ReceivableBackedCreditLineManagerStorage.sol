3b21d960336ef710580a8ee6f7a4da4996df3a68
Jiatu <ljiatu@hotmail.com>
2024-01-04 11:09:16 -0800

    Look up the receivable amount in the receivable contract

--- contracts/credit/ReceivableBackedCreditLineManagerStorage.sol
+++ contracts/credit/ReceivableBackedCreditLineManagerStorage.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.0;
 
+import {IReceivable} from "./interfaces/IReceivable.sol";
+
 contract ReceivableBackedCreditLineManagerStorage {
+    IReceivable public receivableAsset;
+
     /// Mapping from credit hash to the amount of available credit
     mapping(bytes32 => uint96) internal _availableCredits;
 
