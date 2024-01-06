64331d17f08d96494705b82f3d2343e3ff4ec9dd
PlayJoker <jpeng.ustc@gmail.com>
2024-01-05 20:23:11 -0800

    added needed interfaces

---
+++ contracts/interfaces/ITrancheVault.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+import {PoolConfig} from "../PoolConfig.sol";
+
+interface ITrancheVault {
+    function initialize(
+        string memory name,
+        string memory symbol,
+        PoolConfig _poolConfig,
+        uint8 seniorTrancheOrJuniorTranche
+    ) external;
+}
