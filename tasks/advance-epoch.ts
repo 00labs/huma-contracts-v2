8c89c05019704e0a18103df72840c8f33f0d986d
Michael Liu <michael.liu95@gmail.com>
2024-01-08 00:12:46 -0500

    Add tasks folder

---
+++ tasks/advance-epoch.ts
@@ -0,0 +1,45 @@
+import { time } from "@nomicfoundation/hardhat-network-helpers";
+import { task } from "hardhat/config";
+
+task("advanceEpoch", "Advances time in the local blockchain based on options")
+    .addParam(
+        "poolConfigAddr",
+        "The address of the Pool Config whose epoch you wish to advance to next",
+    )
+    .setAction(async (taskArgs: any, hre: any) => {
+        console.log("Advancing to next epoch");
+        let timeToAdvance;
+        const PoolConfig = await hre.ethers.getContractFactory("PoolConfig");
+        const poolConfigContract = PoolConfig.attach(taskArgs.poolConfigAddr);
+        const poolSettings = await poolConfigContract.getPoolSettings();
+        const Calendar = await hre.ethers.getContractFactory("Calendar");
+        const calendarAddress = await poolConfigContract.calendar();
+        const calendarContract = Calendar.attach(calendarAddress);
+        const currentBlockTimestamp = await time.latest();
+
+        // Advance to the next epoch
+        // Get next epoch start time
+        const nextEpochStartTime = await calendarContract.getStartDateOfNextPeriod(
+            poolSettings.payPeriodDuration,
+            currentBlockTimestamp,
+        );
+        timeToAdvance = nextEpochStartTime.toNumber() - currentBlockTimestamp;
+
+        if (timeToAdvance < 0) {
+            console.log("The selected milestone is in the past. Exiting.");
+            return;
+        }
+
+        console.log(
+            `Advancing blockchain by ${timeToAdvance} seconds (~${(
+                timeToAdvance /
+                60 /
+                60 /
+                24
+            ).toPrecision(3)} days)`,
+        );
+
+        // Simulate the passage of time by advancing the time on the Hardhat Network
+        await hre.network.provider.send("evm_increaseTime", [timeToAdvance]);
+        await hre.network.provider.send("evm_mine");
+    });
---
+++ tasks/prepare-tranches-flc-for-withdraw.ts
@@ -0,0 +1,117 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { task } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { FirstLossCover } from "../typechain-types";
+import { CONSTANTS } from "./common";
+
+async function submitRedemptionRequestToTranche(
+    hre: HardhatRuntimeEnvironment,
+    trancheVaultContractAddr: string,
+    redemptionRequester: SignerWithAddress,
+): Promise<void> {
+    const TrancheVault = await hre.ethers.getContractFactory("TrancheVault");
+    const trancheVaultContract = TrancheVault.attach(trancheVaultContractAddr);
+    const redemptionShares = await trancheVaultContract.balanceOf(redemptionRequester.address);
+    const redemptionRequest = await trancheVaultContract
+        .connect(redemptionRequester)
+        .addRedemptionRequest(redemptionShares);
+    await redemptionRequest.wait();
+}
+
+async function withdrawFromFLC(
+    flcContract: FirstLossCover,
+    redemptionRequester: SignerWithAddress,
+): Promise<void> {
+    const redemptionShares = await flcContract.balanceOf(redemptionRequester.address);
+    console.log(
+        redemptionShares,
+        " ",
+        redemptionRequester.address,
+        " ",
+        await flcContract.totalSupply(),
+    );
+    const redemptionRequest = await flcContract
+        .connect(redemptionRequester)
+        .redeemCover(redemptionShares, redemptionRequester.address);
+    await redemptionRequest.wait();
+    const b = await flcContract.balanceOf(redemptionRequester.address);
+    console.log(b, " ", redemptionRequester.address, " ", await flcContract.totalSupply());
+}
+
+task(
+    "prepareTranchesFlcForWithdraw",
+    "Submit withdrawal and redemption requests and prepare pool for closing",
+)
+    .addParam(
+        "poolConfigAddr",
+        "The address of the Pool Config whose epoch you wish to advance to next",
+    )
+    .addParam("poolType", "The type of pool this is (e.g. CreditLine, ArfV2)")
+    .setAction(async (taskArgs: any, hre: HardhatRuntimeEnvironment) => {
+        let poolOwner: SignerWithAddress,
+            poolOwnerTreasury: SignerWithAddress,
+            evaluationAgent: SignerWithAddress,
+            juniorLender: SignerWithAddress,
+            seniorLender: SignerWithAddress,
+            borrowerActive: SignerWithAddress,
+            treasury: SignerWithAddress;
+
+        console.log("Preparing pool for closing");
+        [
+            ,
+            ,
+            treasury,
+            ,
+            ,
+            poolOwner,
+            poolOwnerTreasury,
+            evaluationAgent,
+            ,
+            juniorLender,
+            seniorLender,
+            ,
+            ,
+            borrowerActive,
+        ] = await hre.ethers.getSigners();
+
+        const PoolConfig = await hre.ethers.getContractFactory("PoolConfig");
+        const poolConfigContract = PoolConfig.attach(taskArgs.poolConfigAddr);
+
+        if (taskArgs.poolType === "CreditLine") {
+            console.log("Submitting redemption requests to tranches");
+            const juniorTranche = await poolConfigContract.juniorTranche();
+            const seniorTranche = await poolConfigContract.seniorTranche();
+
+            await poolConfigContract.connect(poolOwner).setPoolOwnerRewardsAndLiquidity(0, 0);
+            await poolConfigContract.connect(poolOwner).setEARewardsAndLiquidity(0, 0);
+
+            // Submit redemption requests
+            await submitRedemptionRequestToTranche(hre, juniorTranche, juniorLender);
+            await submitRedemptionRequestToTranche(hre, juniorTranche, poolOwnerTreasury);
+            await submitRedemptionRequestToTranche(hre, juniorTranche, evaluationAgent);
+            await submitRedemptionRequestToTranche(hre, seniorTranche, seniorLender);
+
+            console.log("Setting FLC to ready for withdraw");
+            const FirstLossCover = await hre.ethers.getContractFactory("FirstLossCover");
+            const flcContracts = await poolConfigContract.getFirstLossCovers();
+            const borrowerFirstLossCoverContract = FirstLossCover.attach(
+                flcContracts[CONSTANTS.BORROWER_LOSS_COVER_INDEX],
+            );
+            const affiliateFirstLossCoverContract = FirstLossCover.attach(
+                flcContracts[CONSTANTS.ADMIN_LOSS_COVER_INDEX],
+            );
+
+            // Set FLC ready to withdraw
+            const Pool = await hre.ethers.getContractFactory("Pool");
+            const poolContract = Pool.attach(await poolConfigContract.pool());
+            await poolContract.connect(poolOwner).setReadyForFirstLossCoverWithdrawal(true);
+
+            await console.log("Withdrawing from FLC");
+            await withdrawFromFLC(borrowerFirstLossCoverContract, borrowerActive);
+            await withdrawFromFLC(affiliateFirstLossCoverContract, poolOwnerTreasury);
+            await withdrawFromFLC(affiliateFirstLossCoverContract, evaluationAgent);
+            await withdrawFromFLC(affiliateFirstLossCoverContract, treasury);
+        } else if (taskArgs.poolType === "ArfV2") {
+            // todo fill in this section
+        }
+    });
