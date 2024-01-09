e79b02d4bbf31b2530ca9bd7a4d7671f6b5eb4c6
Jiatu <ljiatu@hotmail.com>
2024-01-09 14:19:53 -0800

    Revert import changes

--- tasks/advance-epoch.ts
+++ tasks/advance-epoch.ts
@@ -1,5 +1,4 @@
 import { time } from "@nomicfoundation/hardhat-network-helpers";
-import { ethers } from "hardhat";
 import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types/runtime";
 
@@ -11,10 +10,10 @@ task("advanceEpoch", "Advances time in the local blockchain based on options")
     .setAction(async (taskArgs: { poolConfigAddr: string }, hre: HardhatRuntimeEnvironment) => {
         console.log("Advancing to next epoch");
         let timeToAdvance;
-        const PoolConfig = await ethers.getContractFactory("PoolConfig");
+        const PoolConfig = await hre.ethers.getContractFactory("PoolConfig");
         const poolConfigContract = PoolConfig.attach(taskArgs.poolConfigAddr);
         const poolSettings = await poolConfigContract.getPoolSettings();
-        const Calendar = await ethers.getContractFactory("Calendar");
+        const Calendar = await hre.ethers.getContractFactory("Calendar");
         const calendarAddress = await poolConfigContract.calendar();
         const calendarContract = Calendar.attach(calendarAddress);
         const currentBlockTimestamp = await time.latest();
