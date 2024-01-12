63eb486df99047c47935e8e04a7efaaa139f837d
PlayJoker <jpeng.ustc@gmail.com>
2024-01-12 07:28:45 -0800

    added a child contract to expose _intialize PoolConfigCache from

---
+++ contracts/common/mock/PoolConfigCacheChildMock.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+import {PoolConfig} from "../PoolConfig.sol";
+import {PoolConfigCache} from "../PoolConfigCache.sol";
+
+/**
+ * @dev A child contract of PoolConfigCache that exposes internal functions for testing.
+ */
+contract PoolConfigCacheChild is PoolConfigCache {
+    /**
+     * @dev Expose the _initialize function for testing.
+     */
+    function otherInitialize(PoolConfig _poolConfig) external {
+        _initialize(_poolConfig);
+    }
+
+    /**
+     * @dev Override the _updatePoolConfigData function to make
+     * abstract contract PoolConfigCache non-abstract.
+     */
+    function _updatePoolConfigData(PoolConfig _poolConfig) internal view override {
+        address addr = _poolConfig.poolSafe();
+        assert(addr != address(0));
+    }
+}
