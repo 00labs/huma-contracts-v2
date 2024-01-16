f7f3f8484f89994cc9917c9756a7385d77d5ecaa
Jiatu <ljiatu@hotmail.com>
2024-01-16 10:54:06 -0800

    Check pool balance before processing junior redemption requests
    If the pool balance falls below the min threshold, then skip processing junior redemption requests.

--- test/unit/liquidity/EpochManagerTest.ts
+++ test/unit/liquidity/EpochManagerTest.ts
@@ -711,6 +711,58 @@ describe("EpochManager Test", function () {
         );
 
         it(
+            "Should close epochs successfully with the correct LP token prices after processing multiple" +
+                " senior redemption requests, and skipping junior redemption requests because of the available amount" +
+                " falling below the min pool balance threshold",
+            async function () {
+                const availableAssets = await poolSafeContract.getAvailableBalanceForPool();
+                await creditContract.drawdown(ethers.constants.HashZero, availableAssets);
+
+                // Epoch 1
+                const juniorSharesToRedeem = toToken(1628);
+                await juniorTrancheVaultContract
+                    .connect(lender)
+                    .addRedemptionRequest(juniorSharesToRedeem);
+
+                const seniorSharesToRedeem = toToken(357);
+                await seniorTrancheVaultContract
+                    .connect(lender)
+                    .addRedemptionRequest(seniorSharesToRedeem);
+
+                let epochId = await epochManagerContract.currentEpochId();
+                await testCloseEpoch(BN.from(0), BN.from(0));
+                await epochChecker.checkSeniorRedemptionSummaryById(epochId, seniorSharesToRedeem);
+                await epochChecker.checkJuniorRedemptionSummaryById(epochId, juniorSharesToRedeem);
+                await epochChecker.checkSeniorCurrentRedemptionSummary(seniorSharesToRedeem);
+                epochId =
+                    await epochChecker.checkJuniorCurrentRedemptionSummary(juniorSharesToRedeem);
+
+                // Epoch 2
+                // The pool balance falls right at the min threshold so that
+                // no junior redemption request can be processed.
+                const redemptionThreshold =
+                    await epochManagerContract.minPoolBalanceForRedemption();
+                await creditContract.makePayment(
+                    ethers.constants.HashZero,
+                    seniorSharesToRedeem.add(redemptionThreshold),
+                );
+                await testCloseEpoch(seniorSharesToRedeem, BN.from(0));
+                await epochChecker.checkSeniorRedemptionSummaryById(
+                    epochId,
+                    seniorSharesToRedeem,
+                    seniorSharesToRedeem,
+                    seniorSharesToRedeem,
+                );
+                await epochChecker.checkJuniorRedemptionSummaryById(epochId, juniorSharesToRedeem);
+                await epochChecker.checkSeniorCurrentEpochEmpty();
+                await epochChecker.checkJuniorCurrentRedemptionSummary(juniorSharesToRedeem);
+                expect(await poolSafeContract.getAvailableBalanceForPool()).to.equal(
+                    redemptionThreshold,
+                );
+            },
+        );
+
+        it(
             "Should close epochs successfully after processing multiple redemption requests" +
                 " (multiple senior epochs are processed fully," +
                 " some junior epochs are processed fully, some are processed partially" +
